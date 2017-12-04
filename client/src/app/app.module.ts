ff95312613c382dbe999dd663b59a768af42a40f
Marc Pomar <marc@faable.com>
2017-12-04 13:22:24 +0100

    socketio

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,20 +5,23 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { AuthService } from '../services/auth.service';
+import { ChatService } from '../services/chat.service';
 import { LabarraComponent } from './labarra/labarra.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginFormComponent,
-    LabarraComponent
+    LabarraComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [AuthService],
+  providers: [AuthService,ChatService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
